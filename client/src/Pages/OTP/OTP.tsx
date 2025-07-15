cb2ef99eb0733a8ef84c7b021dcb1ded4c60cb4d
Emmanuel <en56434@gmail.com>
2025-07-15 14:40:41 +0100

    feat: enhance OTP component with improved styling and input validation; add expiration message

--- client/src/Pages/OTP/OTP.tsx
+++ client/src/Pages/OTP/OTP.tsx
@@ -4,21 +4,52 @@ function OTP() {
   return (
     <main className="otp-main">
         <div className="otp-inner">
+
+            <div className="otp-title">
             <h1>Enter Verification Code</h1>
             <p>A verification code has been sent to your email address(en***@gmail.com). Please enter the code below to complete your account setup</p>
+            </div>
 
             <form>
                 <div className="otp-input-container">
-                <input type="number" name="otp" id="otp" />
-                <input type="number" name="otp" id="otp" />
-                <input type="number" name="otp" id="otp" />
-                <input type="number" name="otp" id="otp" />
+                <input 
+                maxLength={1}
+                type="number" 
+                name="otp" 
+                required
+                id="otp"
+                max={1}
+                 />
+
+                <input 
+                type="number" 
+                name="otp" 
+                id="otp"
+                maxLength={1} 
+                />
+
+                <input 
+                type="number" 
+                name="otp" 
+                id="otp"
+                maxLength={1} 
+                />
+
+                <input 
+                type="number" 
+                name="otp" 
+                id="otp"
+                maxLength={1} 
+                />
+
                 <input type="number" name="otp" id="otp" />
 
                 </div>
 
                 <button>Verify</button>
             </form>
+
+            <p className="otp-expire">Your verification code will expire in <strong>5 Minutes</strong></p>
         </div>
     </main>
   )
